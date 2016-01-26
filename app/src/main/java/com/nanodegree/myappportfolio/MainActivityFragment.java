f713224a385d51cad2526687b56631001fd73454
Yogesh <yogeshmadaan100@gmail.com>
2016-01-26 16:31:20 +0530

    Changes are 1st review, dimens updated. multi dimensions added

--- app/src/main/java/com/nanodegree/myappportfolio/MainActivityFragment.java
+++ app/src/main/java/com/nanodegree/myappportfolio/MainActivityFragment.java
@@ -8,12 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment implements View.OnClickListener {
-    ViewGroup rootView;
-    Button btnSpotifyStreamer,btnSuperDuo,btnBuildItBigger,btnXYZReader,btnCapstone;
+public class MainActivityFragment extends Fragment {
+    private ViewGroup rootView;
+    @OnClick({R.id.btnBuildItBigger,
+            R.id.btnCapstone,
+            R.id.btnSpotifyStreamer,
+            R.id.btnSuperDuo,
+            R.id.btnXYZReader})
+    public void btnClick(Button button)
+    {
+        showSnackbar(button.getText().toString());
+    }
     public MainActivityFragment() {
     }
 
@@ -21,52 +32,13 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         rootView  = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
-        initViews();
+        ButterKnife.bind(this,rootView);
         return rootView;
-    }
-
-    public void initViews()
-    {
-        btnSpotifyStreamer = (Button) rootView.findViewById(R.id.btnSpotifyStreamer);
-        btnSuperDuo = (Button) rootView.findViewById(R.id.btnSuperDuo);
-        btnBuildItBigger = (Button) rootView.findViewById(R.id.btnBuildItBigger);
-        btnXYZReader = (Button) rootView.findViewById(R.id.btnXYZReader);
-        btnCapstone = (Button) rootView.findViewById(R.id.btnCapstone);
-        btnCapstone.setOnClickListener(this);
-        btnSpotifyStreamer.setOnClickListener(this);
-        btnSuperDuo.setOnClickListener(this);
-        btnBuildItBigger.setOnClickListener(this);
-        btnXYZReader.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.btnBuildItBigger:
-                showSnackbar(getResources().getString(R.string.textBuiltItBigger));
-                break;
-            case R.id.btnCapstone:
-                showSnackbar(getResources().getString(R.string.textCapstone));
-                break;
-            case R.id.btnSpotifyStreamer:
-                showSnackbar(getResources().getString(R.string.textSpotifyRunner));
-                break;
-            case R.id.btnSuperDuo:
-                showSnackbar(getResources().getString(R.string.textSuperDuo));
-                break;
-            case R.id.btnXYZReader:
-                showSnackbar(getResources().getString(R.string.textXYZReader));
-                break;
-            default:
-                break;
-
-        }
     }
 
     public void showSnackbar(String text)
     {
-        Snackbar.make(rootView, "This button will launch my " +text + " app!", Snackbar.LENGTH_LONG)
+        Snackbar.make(rootView,getResources().getString(R.string.text_snackbar_prefix).concat(text).concat(getResources().getString(R.string.text_scnackbar_suffix)), Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
     }
 }
